e1faba7d3eadefa61291fff8ac52e437e2e81eac
priyankagorai66-debug <priyankagorai66@gmail.com>
2026-07-31 14:57:29 +0530

    Add Terraform configuration for Azure resources

---
+++ Implementing-backend/main.tf
@@ -0,0 +1,17 @@
+data "azurerm_resource_group" "rg" {
+  name = "MyRG-terraform"
+}
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = var.container_name
+  storage_account_id    = azurerm_storage_account.storage.id
+  container_access_type = "private"
+
+}
